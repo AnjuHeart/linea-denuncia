9e9d5f2eaf9cd871024c6068886a9f92f8b716d1
AnjuHeart <jybrant.efren@gmail.com>
2023-05-29 23:16:50 -0700

    api paises y estados

--- api/index.js
+++ api/index.js
@@ -9,6 +9,14 @@ router.use(express.urlencoded({extended: false}));
 const apiEmpresas = require('./empresas');
 router.use('/empresas', apiEmpresas);
 
+//  RUTAS API PARA      PAISES
+const apiPaises = require('./paises');
+router.use('/paises', apiPaises);
+
+//  RUTAS API PARA      ESTADOS
+const apiEstados = require('./estados');
+router.use('/estados', apiEstados);
+
 router.get('/*', (req,res) =>{
     res.status(400).send('Request not managed properly');
 });
