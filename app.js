ffee86e8dba23a0c95c51d95a179b36d548a7c07
AnjuHeart <jybrant.efren@gmail.com>
2023-05-30 21:44:48 -0700

    primeros elementos del registro

--- app.js
+++ app.js
@@ -4,12 +4,36 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+
+//RUTA PARA INDICE
 app.get("/", (req,res) =>{
     res.render("index");
 });
 
+//RUTAS PARA REGISTRO
+app.get("/registro" , (req,res) =>{
+    res.render("registro");
+});
+
 const apiRoute = require("./api");
 app.use("/api", apiRoute);
+
+//REQUEST INSIDE SERVER SIDE
+function getJsonFetch(url){
+    return new Promise((resolve, reject) =>{
+        request({
+            url: url,
+            json:true
+        }, (error, response, body) => {
+            if (error) reject(error);
+            if (response.statusCode != 200) {
+                reject('Invalid status code <' + response.statusCode + '>');
+            }
+            resolve(body);
+
+        });
+    });
+}
 
 app.listen(port, ()=> 
     console.log(`Server running at http://localhost:${port}`)
