51c2305a94f54adc8c0c6bf40190f72d5a92b79a
AnjuHeart <jybrant.efren@gmail.com>
2023-05-31 20:24:16 -0700

    registro nueva denuncia y folio

--- api/denuncias.js
+++ api/denuncias.js
@@ -26,9 +26,8 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    console.log(req.body);
     let { empresa, pais, estado, anonimo, nombre_denunciante, correo_denunciante,
-        telefono_denunciante, detalles_text, fecha, contraesna } = req.body;
+        telefono_denunciante, detalles_text, fecha, contrasena, num_centro } = req.body;
     db.any("SELECT * FROM denuncias ORDER BY id ASC")
         .then(rows => {
             const siguienteFolio = rows.length + 1;
@@ -39,9 +38,19 @@ router.post('/', (req, res) => {
             }
             nuevoFolio += siguienteFolio;
 
-            res.send(nuevoFolio);
-
-            console.log(nuevoFolio);
+            db.any(`INSERT INTO denuncias (id, empresa_key, pais_key, estado_key, folio,
+                 contrasena,fecha_evento, detalles, estado, comentarios, denunciante_nombre,
+                 denunciante_correo, denunciante_telefono, numero_centro) 
+                 VALUES (DEFAULT, $1:raw, $2:raw, $3:raw, $4, $5, $6, $7, $8, '{}', $9, $10, $11, $12:raw);`,
+                [empresa,pais,estado,nuevoFolio,contrasena,fecha,detalles_text,"En proceso", nombre_denunciante,
+                correo_denunciante,telefono_denunciante,num_centro])
+            .then(response =>{
+                res.status(200).send(nuevoFolio);
+            })
+            .catch(error =>{
+                console.log(error);
+                res.status(500).send(error);
+            });
         })
         .catch(error => {
             res.status(500).send(error);
